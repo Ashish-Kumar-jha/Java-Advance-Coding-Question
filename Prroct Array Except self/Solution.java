61104b24b2578d5d082f8e005f9d88d229c2a958
ASHISH KUMAR JHA <55866930+Ashish-Kumar-jha@users.noreply.github.com>
2026-06-29 00:06:08 +0530

    product array except itself

---
+++ Prroct Array Except self/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            }
+            maxProfit = (prices[i] - min > maxProfit) ? prices[i] - min : maxProfit;
+        }
+        return maxProfit;
+    }
+}
